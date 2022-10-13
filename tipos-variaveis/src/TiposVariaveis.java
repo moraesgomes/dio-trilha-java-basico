7097472d04d88e5cb60275f94855e078b5ec8520
Haroldo Gomes <moraesgomes@gmail.com>
2022-10-13 17:56:25 -0300

    inclusão-Classe-Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("teste");
+       double salarioMinimo  = 2500;
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+      final double VALOR_DE_PI = 3.14;
+
+    
     }
 }
